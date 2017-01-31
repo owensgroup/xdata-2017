40259302686bf83a5389052bc362b2d3105de396
Carl Yang <ctcyang@ucdavis.edu>
2017-01-31 11:37:27 -0800

    added YC's code

---
+++ src/process_accounts.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ifstream file_list;
+
+    file_list.open("accounts_list.txt");
+    while (!file_list.eof())
+    {
+    }
+    return 0;
+}
+
